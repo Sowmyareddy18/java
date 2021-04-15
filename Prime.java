a187b715649586b49263a8349c9e9f0a62129801
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-04-15 14:30:06 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,18 @@
+class Prime
+{
+public static void main(String args[])
+{
+inti,j;
+for(i=1;i<=100;i++)
+{
+int factor=0;
+for(j=1;j<=i;j++)
+{
+if(i%j==0)
+factor++;
+}
+if(factor==2)
+System.out.print(i+"");
+}
+}
+}
